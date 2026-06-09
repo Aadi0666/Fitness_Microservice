858bb25107b53c2bcaecf86421894d0626e4fe44
Aadi0666 <adityatyagi0110@gmail.com>
2026-06-10 00:21:44 +0530

    Initial commit

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,16 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    CALISTHENICS,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    ZUMBA,
+    TRACKING,
+}

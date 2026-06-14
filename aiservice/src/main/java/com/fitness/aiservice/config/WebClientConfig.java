1c6027f37bd565fddd7d91c55d7407b9a70ae412
Aadi0666 <adityatyagi0110@gmail.com>
2026-06-15 01:31:10 +0530

    Generating Ai Driven Recommendations

---
+++ aiservice/src/main/java/com/fitness/aiservice/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.fitness.aiservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
